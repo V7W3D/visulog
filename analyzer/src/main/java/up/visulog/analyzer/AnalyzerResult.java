ee4f93f615ad97daeb97054047c28947ada3727d
SIDI AHMED Ahmed <sidiahmedahmed2001@gmail.com>
2021-10-19 06:05:40 +0200

    Options --justSaveConfigFile et --loadConfigFile

--- analyzer/src/main/java/up/visulog/analyzer/AnalyzerResult.java
+++ analyzer/src/main/java/up/visulog/analyzer/AnalyzerResult.java
@@ -1,6 +1,8 @@
 package up.visulog.analyzer;
 
 import java.util.List;
+import java.io.FileWriter;  
+import java.io.IOException;
 
 public class AnalyzerResult {
     public List<AnalyzerPlugin.Result> getSubResults() {
@@ -18,7 +20,16 @@ public class AnalyzerResult {
         return subResults.stream().map(AnalyzerPlugin.Result::getResultAsString).reduce("", (acc, cur) -> acc + "\n" + cur);
     }
 
-    public String toHTML() {
-        return "<html><body>"+subResults.stream().map(AnalyzerPlugin.Result::getResultAsHtmlDiv).reduce("", (acc, cur) -> acc + cur) + "</body></html>";
+    public void toHTML() {
+        String codeHTML = "<html><body>"+subResults.stream().map(AnalyzerPlugin.Result::getResultAsHtmlDiv).reduce("", (acc, cur) -> acc + cur) + "</body></html>";
+
+        try {
+            FileWriter myWriter = new FileWriter("../resultat.html");
+            myWriter.write(codeHTML);
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
