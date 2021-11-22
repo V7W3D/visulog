92612db32acc5162150e3d0efdfbab952eefa565
khalifa <khalifadione36@gmail.com>
2021-11-22 19:43:48 +0100

    representation graphique des commits par jour pour l'annee

--- analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPlugin.java
@@ -113,28 +113,20 @@ public class CountCommitsPerAuthorPlugin implements AnalyzerPlugin {
             String date_jour = DateTimeFormatter.ofPattern("d", Locale.ENGLISH).format(ldt);
             String annee = DateTimeFormatter.ofPattern("yyyy", Locale.ENGLISH).format(ldt);
             System.out.println("Liste des commits du jour: ");
-
+            int mbTotalcommitsjour = 0;
             for (var item1 : getCommitsPerAuth().entrySet()) {
-                //startsWith("")
-                //if (item1.getKey().startsWith(DateTimeFormatter.ofPattern("EEE MM dd", Locale.ENGLISH).format(ldt))) {
-                    /*if(item1.getKey().contains(DateTimeFormatter.ofPattern("EEE", Locale.ENGLISH).format(ldt))
-                            && item1.getKey().contains(DateTimeFormatter.ofPattern("MM", Locale.ENGLISH).format(ldt))
-                            && item1.getKey().contains(DateTimeFormatter.ofPattern("dd", Locale.ENGLISH).format(ldt))
-                            && item1.getKey().contains(DateTimeFormatter.ofPattern("yyyy", Locale.ENGLISH).format(ldt))) {
-                            System.out.println(item1.getKey()+" "+item1.getValue()+" commits");
-                        //add to my result list
-                    }*/
                 //Thu Aug 27 00:35:19 2020 +0200
                 if (item1.getKey().contains(jour)
                         && item1.getKey().contains(mois)
                         && item1.getKey().contains(date_jour)
                         && item1.getKey().contains(annee)) {
-                    System.out.println("-"+item1.getKey() + ": " + item1.getValue() + " commit(s)");
+                    mbTotalcommitsjour += item1.getValue();
+                    System.out.println("-"+item1.getKey() + " : " + item1.getValue() + " commit(s)");
                     //add to my result list
                 }
 
-
             }
+            System.out.println("Total commit(s) jour : "+mbTotalcommitsjour);
             System.out.println(" ");
 
 
@@ -149,18 +141,18 @@ public class CountCommitsPerAuthorPlugin implements AnalyzerPlugin {
                 }*/
 
             // Enregidtrement des dates de commits et le nombre de commits pour chaque date dans un fichier
-            File fil = new File("/home/khalifa/Documents/L3/Donnees.txt");
-            BufferedWriter bf = null;
+            File fildc = new File("/home/khalifa/Documents/L3/Donnees.txt");
+            File fild = new File("/home/khalifa/Documents/L3/Date.txt");
+            BufferedWriter bfdc = null;
             try {
-                bf = new BufferedWriter(new FileWriter(fil));
+                bfdc = new BufferedWriter(new FileWriter(fildc));
                 //
                 for (Map.Entry<String, Integer> entry: commitsdate.entrySet()) {
-                    bf.write(entry.getKey() + ":" + entry.getValue()+" commit(s)");
-
-                    bf.newLine();
+                    bfdc.write(entry.getKey() + " : " + entry.getValue()+" commit(s)");
+                    bfdc.newLine();
                 }
 
-                bf.flush();
+                bfdc.flush();
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -168,7 +160,45 @@ public class CountCommitsPerAuthorPlugin implements AnalyzerPlugin {
 
                 try {
                     //always close the writer
-                    bf.close();
+                    bfdc.close();
+                } catch (Exception e) {
+                }
+            }
+
+
+
+            File filecomdeuM = new File("/home/khalifa/Documents/L3/commits2021.txt");
+            File filecomdeuMdate = new File("/home/khalifa/Documents/L3/Date.txt");
+            BufferedWriter bfcomdeuM= null;
+            BufferedWriter bfcomdeuMdate= null;
+            try {
+                bfcomdeuM = new BufferedWriter(new FileWriter(filecomdeuM));
+                bfcomdeuMdate = new BufferedWriter(new FileWriter(filecomdeuMdate));
+                //
+
+
+                        for (Map.Entry<String, Integer> entry: commitsdate.entrySet()) {
+                            if (entry.getKey().contains(DateTimeFormatter.ofPattern("yyyy", Locale.ENGLISH).format(ldt))) {
+                                bfcomdeuM.write(entry.getKey() + " : " + entry.getValue()+" commit(s)");
+                                bfcomdeuM.newLine();
+                                bfcomdeuMdate.write(entry.getKey());
+                                bfcomdeuMdate.newLine();
+
+                            }
+                        }
+
+                bfcomdeuM.flush();
+                bfcomdeuM.flush();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+
+                try {
+                    //always close the writer
+                    bfcomdeuM.close();
+                    bfcomdeuMdate.close();
+
                 } catch (Exception e) {
                 }
             }
